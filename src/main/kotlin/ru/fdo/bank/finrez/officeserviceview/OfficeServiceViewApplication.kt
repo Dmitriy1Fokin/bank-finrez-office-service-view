5e1a5b36baf4513891803a864f5e5369f2819165
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-22 16:44:01 +0300

    [BF-73] first commit. Init empty project.

---
+++ src/main/kotlin/ru/fdo/bank/finrez/officeserviceview/OfficeServiceViewApplication.kt
@@ -0,0 +1,11 @@
+package ru.fdo.bank.finrez.officeserviceview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OfficeServiceViewApplication
+
+fun main(args: Array<String>) {
+	runApplication<OfficeServiceViewApplication>(*args)
+}
---
+++ src/test/kotlin/ru/fdo/bank/finrez/officeserviceview/OfficeServiceViewApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.fdo.bank.finrez.officeserviceview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OfficeServiceViewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
